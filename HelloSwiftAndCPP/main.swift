6a675985ef65c9714c7ecdb2d6ca04bb3881f963
Ron Olson <rolson@lbpc.com>
2014-06-12 08:39:23 -0500

    Initial Commit

---
+++ HelloSwiftAndCPP/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloSwiftAndCPP
+//
+//  Created by Ron Olson on 6/12/14.
+//  Copyright (c) 2014 Ron Olson. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
